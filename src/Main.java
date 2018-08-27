92113c76efc39b66ecca06ed1f06e0593df7c017
phreemason <mr.omarmason@gmail.com>
2018-08-27 01:07:20 -0400

    variable declarations and if statements

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,21 @@ public class Main {
     public static void main(String[] args) {
     // write your code here
         System.out.println("Hello World");
+        int myNumber;
+        myNumber = 100;
+        int myOtherNumber = myNumber;
+        float lastNumber = 14;
+        System.out.println(myNumber);
+        System.out.println(myOtherNumber);
+        System.out.println(lastNumber);
+        myNumber++;
+
+        if (lastNumber < myNumber) {
+            System.out.println("the bigger number is:");
+            System.out.println(myNumber);
+        } else {
+            System.out.println("the bigger number is:");
+            System.out.println(lastNumber);
+        }
     }
 }
